3543dd17c79765d01851b0f49e5ff7934f875cff
ktzanev <kroum.tzanev@math.univ-lille1.fr>
2017-04-02 21:41:12 +0200

    first version under VueJS

---
+++ main.js
@@ -0,0 +1,223 @@
+var vm = new Vue({
+  el: "#app",
+  data: {
+    aPolygon : [],  // the polygon like [ {cx : 0, cy :0, selected : false}, ...]
+    dPolygon : [], // and it's dual
+    aIsMaster : true, // decide the master / slave relation between A and D
+    graphSize : 7,
+    vertexSize : .05,
+    edgeWidth : .035,
+    integerPointSize : .049,
+    precision : .5
+  },// end data
+  // ------------------
+  watch: {
+    aPolygon : {
+      handler : function (newA) {
+        if (this.aIsMaster)
+          this.dPolygon = this.dualPolygon(this.aLines);
+      },
+      deep : true
+    },
+    dPolygon : {
+      handler : function (newD) {
+        if (!this.aIsMaster)
+          this.aPolygon = this.dualPolygon(this.dLines);
+      },
+      deep : true
+    }
+  },
+  computed: {
+    volumeAPolygon : function () {
+      return this.volumePolygon(this.aPolygon);
+    },
+    volumeDPolygon : function () {
+      return this.volumePolygon(this.dPolygon);
+    },
+    roundedAPolygon : function () {
+      return this.roundedPolygon(this.aPolygon);
+    },
+    roundedDPolygon : function () {
+      return this.roundedPolygon(this.dPolygon);
+    },
+    aLines: function () {
+      return this.edgesOfPolygon(this.aPolygon);
+    }, // end aLines()
+    dLines: function () {
+      return this.edgesOfPolygon(this.dPolygon);
+    }, // end dLines
+    graphPos: function graphPos() {
+      var size = this.graphSize;
+      var half = size / 2;
+      return {
+        viewBox: [-half, -half, size, size].join(" "),
+        width: size
+      };
+    },
+  }, // end computed
+  created: function () {
+    this.aPolygon = [
+      {
+        cx: 1,
+        cy: 0,
+        selected : false
+      },
+      {
+        cx: 0,
+        cy: 1,
+        selected : false
+      },
+      {
+        cx: -1,
+        cy: -1,
+        selected : false
+      }
+    ];
+  }, // end created
+  // ------------------
+  methods: {
+    // ------------------
+    // Dual methods
+    // ------------------
+    dualPointFromSegment: function(s) {
+      var d=s.x1*s.y2-s.y1*s.x2;
+      return d ? { cx : (s.y2-s.y1)/d, cy : (s.x1-s.x2)/d} : null;
+    },
+    dualPolygon : function (line) {
+      var n = line.length; // number of segments
+      var dual = []; // the dual polygone will be stored inside
+      var dp; // temp dual point
+
+      for (var i = 0; i < n; i++) {
+        dp = this.dualPointFromSegment(line[i]);
+        if (!dp) return [];
+        dp["selected"] = false;
+        dual.push(dp);
+      }
+
+      return dual;
+    },
+    // ------------------
+    // Accessories
+    // ------------------
+    edgesOfPolygon : function (poly){
+      var n = poly.length; // number of points
+      var edges = new Array(); // will contain all the edges like {x1,y1,x2,y2}
+
+      for (var i = 0, j = 1; i < n; j = (++i+1) % n) {
+        edges.push({
+          x1 : poly[i].cx,
+          y1 : poly[i].cy,
+          x2 : poly[j].cx,
+          y2 : poly[j].cy
+        });
+      }
+
+      return edges;
+    }, // end aLines()
+    roundPolygon : function (poly){
+      var n = poly.length; // number of points
+
+      for (var i = 0; i < n; i++) {
+        poly[i].cx = Math.round(poly[i].cx/this.precision)*this.precision;
+        poly[i].cy = Math.round(poly[i].cy/this.precision)*this.precision;
+      }
+    }, // end roundedPolygon
+    volumePolygon : function (poly){
+      var n = poly.length; // number of points
+      var v = 0;
+
+      for (var i = 0, j = 1; i < n; j = (++i+1) % n) {
+        v += poly[i].cx * poly[j].cy - poly[j].cx * poly[i].cy;
+      }
+
+      return Math.abs(v/2);
+    }, // end volumeOfPolygon()
+    // ------------------
+    addPoint: function addPoint(evt, i, useA) {
+      if(!evt.shiftKey)
+        return;
+
+      var getPos = getMousePos;
+      var svg = evt.currentTarget.closest("svg");
+      var point = svg.createSVGPoint();
+      var transform = svg.getScreenCTM().inverse();
+      var xPolygon = useA ? this.aPolygon : this.dPolygon;
+
+      getPos(evt, point);
+      var newPt = point.matrixTransform(transform);
+      xPolygon.splice((i+1) % xPolygon.length, 0, {
+          cx: newPt.x,
+          cy: newPt.y,
+          selected : false
+        });
+    }, // end addPoint
+    // ------------------
+    startMove: function startMove(evt, vertex, useA) {
+      var touch = evt.type === "touchstart";
+      if (!touch && evt.button !== 0) return;
+      var events = touch ? {
+        move: "touchmove",
+        stop: "touchend"
+      } : {
+        move: "mousemove",
+        stop: "mouseup"
+      };
+      var getPos = touch ? getTouchPos : getMousePos;
+      var svg = evt.currentTarget.closest("svg");
+      var point = svg.createSVGPoint();
+      var transform = svg.getScreenCTM().inverse();
+      var xPolygon = useA ? this.aPolygon : this.dPolygon;
+
+      this.aIsMaster = useA;
+
+      var moving = true;
+      var newPt, oldPt;
+      getPos(evt, point);
+      oldPt = point.matrixTransform(transform);
+
+      vertex.selected = true;
+
+      var updateFn = function updateFn() {
+        if (moving) requestAnimationFrame(updateFn);
+
+        // Map the screen pixels back to svg coords
+        newPt = point.matrixTransform(transform);
+        var dx = newPt.x - oldPt.x;
+        var dy = newPt.y - oldPt.y;
+        oldPt = newPt;
+        for (var i = 0; i < xPolygon.length; i++)
+          if (xPolygon[i].selected) {
+            xPolygon[i].cx += dx;
+            xPolygon[i].cy += dy;
+          };
+      };
+      var moveFn = function moveFn(evt) {
+        return getPos(evt, point);
+      };
+      var stopFn = function stopFn(evt) {
+        moving = false;
+        vertex.selected=event.ctrlKey;
+        svg.removeEventListener(events.move, moveFn);
+        svg.removeEventListener(events.stop, stopFn);
+      };
+
+      requestAnimationFrame(updateFn);
+      moveFn(evt);
+
+      svg.addEventListener(events.move, moveFn);
+      svg.addEventListener(events.stop, stopFn);
+    } // end startMove
+    // ------------------
+  } // end methods
+});
+
+function getMousePos(mouseEvent, point) {
+  point.x = mouseEvent.clientX;
+  point.y = mouseEvent.clientY;
+}
+
+function getTouchPos(touchEvent, point) {
+  point.x = touchEvent.touches[0].clientX;
+  point.y = touchEvent.touches[0].clientY;
+}
